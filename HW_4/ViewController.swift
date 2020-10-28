e6c35fba04a7fc672feebd98879c21eaab74b769
Alexander <alex.pismenskiy@gmail.com>
2020-10-28 12:12:45 +0300

    Initial Commit

---
+++ HW_4/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  HW_4
+//
+//  Created by Alexander Pismenskiy on 28.10.2020.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
